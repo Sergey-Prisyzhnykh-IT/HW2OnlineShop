0dbe28ddec8441c4f1c97385a1b8a23b4b08c668
Sergey <joker3839@gmail.com>
2021-11-26 00:32:18 +0300

    Абстракция времени

--- HW2OnlineShop/Product/ProductCatalog.cs
+++ HW2OnlineShop/Product/ProductCatalog.cs
@@ -1,7 +1,16 @@
-﻿namespace HW2OnlineShop
+﻿using HW2OnlineShop.Time;
+
+namespace HW2OnlineShop
 {
     public class ProductCatalog : IProductCatalog
     {
+        IRealTime _time;
+
+        public ProductCatalog(IRealTime time)
+        {
+            _time = time;
+        }
+
         private readonly IReadOnlyCollection<Product> _products = new Product[]
         {
             new ("Куртка", 1000m),
@@ -11,7 +20,7 @@
 
         public IReadOnlyCollection<Product> GetProductCatalog()
         {
-            if (DateTime.UtcNow.DayOfWeek is DayOfWeek.Saturday or DayOfWeek.Sunday)
+            if (_time.GetRealTime().DayOfWeek is DayOfWeek.Saturday or DayOfWeek.Sunday)
                 return _products.Select(it => it with { Price = it.Price * 1.5m }).ToList();
             else
                 return _products;
--- HW2OnlineShop/Program.cs
+++ HW2OnlineShop/Program.cs
@@ -2,8 +2,9 @@
 using HW2OnlineShop.Time;
 using Microsoft.AspNetCore.Http.Json;
 
-var builder = WebApplication.CreateBuilder(args);
 
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddSingleton<IRealTime, RealTime>();
 builder.Services.AddSingleton<IProductCatalog, ProductCatalog>();
 
 builder.Services.Configure<JsonOptions>(options => 
